e82622aaa3af2f5e4f8a2f4cb14f22c561bc5b26
alberme <albertm@alphaworks.tech>
2023-06-25 02:12:53 -0700

    lots more pwogwess

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./**/*.{html,js}"],
-  mode: 'jit',
+  mode: "jit",
   theme: {
     extend: {
       content: {
-        duggyLeftSide: 'url("./pics/duggywave-leftside.png")',
-        duggyRightSide: 'url("./pics/duggywave-rightside.png")',
+        duggyLeftSide: 'url("../pics/duggywave-leftside.png")',
+        duggyRightSide: 'url("../pics/duggywave-rightside.png")',
       },
       animation: {
-        marquee: 'marquee 25s linear infinite',
-        marquee2: 'marquee2 25s linear infinite',
+        marquee: "marquee 25s linear infinite",
+        marquee2: "marquee2 25s linear infinite",
       },
       keyframes: {
         marquee: {
-          '0%': { transform: 'translateX(0%)' },
-          '100%': { transform: 'translateX(-100%)' },
+          "0%": { transform: "translateX(0%)" },
+          "100%": { transform: "translateX(-100%)" },
         },
         marquee2: {
-          '0%': { transform: 'translateX(100%)' },
-          '100%': { transform: 'translateX(0%)' },
+          "0%": { transform: "translateX(100%)" },
+          "100%": { transform: "translateX(0%)" },
         },
       },
     },
   },
   plugins: [],
-}
-
+};
